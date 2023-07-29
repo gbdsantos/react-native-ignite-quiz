5e157caef0d89afcb65e2f4a6d6185d2595558b5
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-07-28 23:36:10 -0300

    🎨 styles(mobile): animate the progress bar

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,6 +1,13 @@
 import { View } from 'react-native';
 
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated';
+
 import { styles } from './styles';
+import { useEffect } from 'react';
 
 interface Props {
   total: number;
@@ -10,9 +17,21 @@ interface Props {
 export function ProgressBar({ total, current }: Props) {
   const percentage = Math.round((current / total) * 100);
 
+  const sharedProgress = useSharedValue(percentage);
+
+  const styledAnimated = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`
+    }
+  })
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(percentage);
+  }, [current]);
+
   return (
     <View style={styles.track}>
-      <View style={[styles.progress, { width: `${percentage}%` }]} />
+      <Animated.View style={[styles.progress, styledAnimated]} />
     </View>
   );
 }
