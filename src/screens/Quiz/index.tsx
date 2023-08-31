479d2b222592fe37ac792b6a579fcee3048050f1
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-08-30 23:32:37 -0300

    🎨 styles(mobile): animation of discard

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -33,6 +33,8 @@ interface Params {
 
 type QuizProps = typeof QUIZ[0];
 
+const CARD_INCLINATION = 10;
+
 export function Quiz() {
   const [points, setPoints] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
@@ -170,11 +172,26 @@ export function Quiz() {
   const onPan = Gesture
     .Pan()
     .onUpdate((event) => {
-      cardPosition.value = (event.translationX);
+      const moveToLeft = event.translationX < 0;
+
+      if (moveToLeft) {
+        cardPosition.value = (event.translationX);
+      }
     })
     .onEnd(() => {
       cardPosition.value = withTiming(0);
-    })
+    });
+
+  const dragStyles = useAnimatedStyle(() => {
+    const rotateZ = cardPosition.value / CARD_INCLINATION;
+
+    return {
+      transform: [
+        { translateX: cardPosition.value },
+        { rotateZ: `${rotateZ}deg` }
+      ]
+    }
+  });
 
   if (isLoading) {
     return <Loading />
@@ -208,7 +225,7 @@ export function Quiz() {
         </Animated.View>
 
         <GestureDetector gesture={onPan}>
-          <Animated.View style={shakeStyleAnimated}>
+          <Animated.View style={[shakeStyleAnimated, dragStyles]}>
             <Question
               key={quiz.questions[currentQuestion].title}
               question={quiz.questions[currentQuestion]}
