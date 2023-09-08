681b4b821091c52326b82f01219e989d822b1504
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-09-08 02:33:36 -0300

    🎨 styles(mobile): take the stars to the canvas

--- src/components/Stars/styles.ts
+++ src/components/Stars/styles.ts
@@ -2,7 +2,14 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
+    justifyContent: "flex-end",
     alignItems: "center",
+  },
+
+  canvas: {
+    position: "absolute",
+    height: 249,
+    width: 257,
+    zIndex: 1
   }
 });
